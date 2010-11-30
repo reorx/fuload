2bc2b2b85e95286ccad0c7b0f8dd161d5c51d1f3
zny2008 <zny2008@b9611eb2-d0e1-e8f3-2d0a-be6844a88301>
2010-11-30 16:30:41 +0000

    ci

--- src/slave/ctrl/fl_slave_ctrl.py
+++ src/slave/ctrl/fl_slave_ctrl.py
@@ -1,5 +1,33 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import subprocess
+from subprocess import Popen
+import signal
 
-if __name__ == '__main__':
-    print 'ok'
+childs = []
+
+def handler(signo, frame):
+    global childs
+    for child in childs:
+        try:
+            child.send_signal(signal.SIGINT)
+        except:
+            pass
+
+def main():
+    global childs
+
+    for i in range(0,10):
+        p = Popen(["./fl_slave_worker"])
+        childs.append(p)
+
+    signal.signal(signal.SIGINT, handler)
+
+    #for child in childs:
+        #child.send_signal(signal.SIGUSR1)
+
+    for child in childs:
+        child.wait()
+
+if __name__ == "__main__":
+    main()
