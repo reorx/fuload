8669bbadc43d851eaa7fab2801a08e947b83c70d
zny2008 <zny2008@b9611eb2-d0e1-e8f3-2d0a-be6844a88301>
2010-11-30 14:02:25 +0000

    ci

--- trunk/src/slave/test/signal/test_signal_recv.cpp
+++ trunk/src/slave/test/signal/test_signal_recv.cpp
@@ -3,9 +3,36 @@
 #include <vector>
 #include <set>
 #include <map>
+#include <error.h>
+#include <errno.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <poll.h>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <strings.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <signal.h>
 using namespace std;
+
+void ouch(int sig)
+{
+    printf("good-%d\n",sig);
+}
 int main(int argc, const char *argv[])
 {
-    
+    signal(SIGUSR1,ouch);
+    while(1)
+    {
+        printf("hello world\n");
+        sleep(1);
+    }
     return 0;
 }
--- trunk/src/slave/test/signal/test_signal_send.py
+++ trunk/src/slave/test/signal/test_signal_send.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import os
+import signal
+
+ps_output = os.popen("ps x | grep test_signal_recv").readline()
+fd = ps_output.strip().split(' ')[0]
+
+print fd
+os.kill(int(fd),signal.SIGUSR1)
