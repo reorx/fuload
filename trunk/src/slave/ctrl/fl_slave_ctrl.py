a8f6662b5cdee5c59f02933019fc4c06d1463b3a
zny2008 <zny2008@b9611eb2-d0e1-e8f3-2d0a-be6844a88301>
2010-11-30 17:09:35 +0000

    ci

--- trunk/src/slave/ctrl/fl_slave_ctrl.py
+++ trunk/src/slave/ctrl/fl_slave_ctrl.py
@@ -1,20 +1,28 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import sys
+import os
 import subprocess
 from subprocess import Popen
 import signal
+from os.path import abspath, dirname, join
+
+from daemon import Daemon
 
 childs = []
+
+mpath = abspath(dirname(__file__))
 
 def handler(signo, frame):
     global childs
     for child in childs:
         try:
-            child.send_signal(signal.SIGINT)
+            child.send_signal(signo)
         except:
             pass
 
-def main():
+def fork_wait():
+    os.chdir(mpath) 
     global childs
 
     for i in range(0,10):
@@ -22,6 +30,7 @@ def main():
         childs.append(p)
 
     signal.signal(signal.SIGINT, handler)
+    signal.signal(signal.SIGTERM, handler)
 
     #for child in childs:
         #child.send_signal(signal.SIGUSR1)
@@ -29,5 +38,23 @@ def main():
     for child in childs:
         child.wait()
 
+class SlaveDaemon(Daemon):
+    def _run(self):
+        fork_wait()
+
 if __name__ == "__main__":
-    main()
+    daemon = SlaveDaemon('/tmp/daemon-slave-ctrl.pid',stdin='/tmp/fl_in',stdout='/tmp/fl_out',stderr='/tmp/fl_err')
+    if len(sys.argv) == 2:
+        if 'start' == sys.argv[1]:
+            daemon.start()
+        elif 'stop' == sys.argv[1]:
+            daemon.stop()
+        elif 'restart' == sys.argv[1]:
+            daemon.restart()
+        else:
+            print "Unknown command"
+            sys.exit(2)
+        sys.exit(0)
+    else:
+        print "usage: %s start|stop|restart" % sys.argv[0]
+        sys.exit(2)
