bf93268d656e5df7d30ecf1a99b3b4492a5a7755
zny2008 <zny2008@b9611eb2-d0e1-e8f3-2d0a-be6844a88301>
2010-12-04 05:39:07 +0000

    ci

--- src/master/mysite/report/views.py
+++ src/master/mysite/report/views.py
@@ -6,39 +6,39 @@ except ImportError:
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
-from report_handler import ReportHandler
+from report_handler import ReportUploadHandler
 
 def HandleReportUpload(request,reportId):
     #if request.method != 'POST':
         #return HttpResponse(json.dumps({"ret":-1,"msg":"please use post method"}))
 
-    if 'reportinfo' not in request.POST:
-        return HttpResponse(json.dumps({"ret":-1,"msg":"no reportinfo in postdata"}))
-    slaveReport = request.POST['reportinfo'];
+    #if 'reportinfo' not in request.POST:
+        #return HttpResponse(json.dumps({"ret":-1,"msg":"no reportinfo in postdata"}))
+    #slaveReport = request.POST['reportinfo'];
 
-    #slaveReport = {
-            #'allTimeMsStat':1000,
-            #'sucTimeMsStat':900,
-            #'errTimeMsStat':100,
-            #'allReqNum':10,
-            #'sucReqNum':8,
-            #'errReqNum':2,
-            #'timemap':{
-                #5:10,
-                #10:15,
-                #100:20
-                #},
-            #'retmap':{
-                #0:1000,
-                #1:10,
-                #1000:22
-                #}
-            #}
-    #slaveReport = json.dumps(slaveReport)
+    slaveReport = {
+            'allTimeMsStat':1000,
+            'sucTimeMsStat':900,
+            'errTimeMsStat':100,
+            'allReqNum':10,
+            'sucReqNum':8,
+            'errReqNum':2,
+            'timemap':{
+                5:10,
+                10:15,
+                100:20
+                },
+            'retmap':{
+                0:1000,
+                1:10,
+                1000:22
+                }
+            }
+    slaveReport = json.dumps(slaveReport)
 
     #such as 127.0.0.1
     clientIp = request.META['REMOTE_ADDR']
-    handler = ReportHandler(int(reportId), clientIp, slaveReport)
+    handler = ReportUploadHandler(int(reportId), clientIp, slaveReport)
     ret = handler.handle_report()
     if ret is False:
         return HttpResponse(json.dumps({"ret":-1,"msg":"set_report failed"}))
