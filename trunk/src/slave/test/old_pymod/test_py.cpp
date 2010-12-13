33e1083bc97ca7214ed375f8526adfdd7b064ade
zny2008 <zny2008@b9611eb2-d0e1-e8f3-2d0a-be6844a88301>
2010-12-13 16:00:11 +0000

    ci

--- trunk/src/slave/test/old_pymod/test_py.cpp
+++ trunk/src/slave/test/old_pymod/test_py.cpp
@@ -1,4 +1,67 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
 #include<Python.h>//前面所做的一切配置都是为了调用这个头文件和相关库
+using namespace std;
+string log_python_exception()
+{
+    std::string strErrorMsg;
+    if (!Py_IsInitialized())
+    {
+        strErrorMsg = "Python 运行环境没有初始化！";
+        return strErrorMsg;
+    }
+
+    if (PyErr_Occurred() != NULL)
+    {
+        PyObject *type_obj, *value_obj, *traceback_obj;
+        PyErr_Fetch(&type_obj, &value_obj, &traceback_obj);
+        if (value_obj == NULL)
+            return strErrorMsg;
+
+        PyErr_NormalizeException(&type_obj, &value_obj, 0);
+        if (PyString_Check(PyObject_Str(value_obj)))
+        {
+            strErrorMsg = PyString_AsString(PyObject_Str(value_obj));
+        }
+
+        if (traceback_obj != NULL)
+        {
+            strErrorMsg += "Traceback:";
+
+            PyObject * pModuleName = PyString_FromString("traceback");
+            PyObject * pTraceModule = PyImport_Import(pModuleName);
+            Py_XDECREF(pModuleName);
+            if (pTraceModule != NULL)
+            {
+                PyObject * pModuleDict = PyModule_GetDict(pTraceModule);
+                if (pModuleDict != NULL)
+                {
+                    PyObject * pFunc = PyDict_GetItemString(pModuleDict,"format_exception");
+                    if (pFunc != NULL)
+                    {
+                        PyObject * errList = PyObject_CallFunctionObjArgs(pFunc,type_obj,value_obj, traceback_obj,NULL);
+                        if (errList != NULL)
+                        {
+                            int listSize = PyList_Size(errList);
+                            for (int i=0;i < listSize;++i)
+                            {
+                                strErrorMsg += PyString_AsString(PyList_GetItem(errList,i));
+                            }
+                        }
+                    }
+                }
+                Py_XDECREF(pTraceModule);
+            }
+        }
+        Py_XDECREF(type_obj);
+        Py_XDECREF(value_obj);
+        Py_XDECREF(traceback_obj);
+    }
+    return strErrorMsg;
+}
 int main()
 {
     Py_Initialize();//使用python之前，要调用Py_Initialize();这个函数进行初始化
@@ -28,15 +91,26 @@ int main()
     }
     else
     {
-        printf("result is null");
+        printf("1.result is null\n");
     }
+    printf("process\n");
 
     pFunc= PyObject_GetAttrString(pModule, "fuload_handle_process");//这里是要调用的函数名
     //PyObject_CallFunction(pFunc,"{s:i,s:i}","abc", 123, "def", 456);//调用函数
     PyObject * t_dict = PyDict_New();
     PyDict_SetItemString(t_dict,"first",Py_BuildValue("s", "hello"));
     PyDict_SetItemString(t_dict,"second",Py_BuildValue("s", "world"));
-    PyObject_CallFunctionObjArgs(pFunc,t_dict,NULL);
+    objResult = PyObject_CallFunctionObjArgs(pFunc,t_dict,NULL);
+    if (!objResult)
+    {
+        printf("2.result is null");
+        printf("%s\n",log_python_exception().c_str());
+    }
+    else
+    {
+        ret = PyInt_AsLong(objResult);
+        printf("ret:%d\n",ret);
+    }
 
     pFunc= PyObject_GetAttrString(pModule, "fuload_handle_fini");//这里是要调用的函数名
     PyObject_CallFunction(pFunc, NULL);//调用函数
