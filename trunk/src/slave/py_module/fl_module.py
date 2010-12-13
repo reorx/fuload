deff3ebf706ae3e8754cf46b8a8f0c54ef68e361
zny2008 <zny2008@b9611eb2-d0e1-e8f3-2d0a-be6844a88301>
2010-12-13 12:11:29 +0000

    s

---
+++ trunk/src/slave/py_module/fl_module.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+#=============================================================================
+#  Author:          dantezhu - http://www.vimer.cn
+#  Email:           zny2008@gmail.com
+#  FileName:        fl_module.py
+#  Description:     给python用的module主文件
+#  Version:         1.0
+#  LastChange:      2010-12-13 18:45:10
+#  History:         
+#=============================================================================
+'''
+def fuload_handle_init():
+    print 'init'
+    return 0
+
+def fuload_handle_process(mapParams):
+    print 'process'
+    print repr(mapParams)
+    return 0
+
+def fuload_handle_fini():
+    print 'fini'
+    return 0
