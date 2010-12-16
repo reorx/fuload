d7f54be2a8175b85750fa2806b6f5fd9784e1a44
zny2008 <zny2008@b9611eb2-d0e1-e8f3-2d0a-be6844a88301>
2010-12-16 15:41:45 +0000

    将log类变成打印一个文件

--- trunk/src/slave/test/test_log/main.cpp
+++ trunk/src/slave/test/test_log/main.cpp
@@ -7,10 +7,11 @@
 using namespace std;
 int main(int argc, const char *argv[])
 {
-    log_init(LM_ERROR,"./","log",200);
+    log_init(LM_DEBUG,"./log/","worker",200);
     for (int i = 0; i < 100; i++)
     {
-        error_log("error : %d",i);
+        debug_log("debug is : %d",i);
+        error_log("error is : %d",i);
     }
     return 0;
 }
