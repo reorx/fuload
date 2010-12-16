b7a882b7759b03727b61674adb6f1b5d0c5f9098
zny2008 <zny2008@b9611eb2-d0e1-e8f3-2d0a-be6844a88301>
2010-12-16 15:41:45 +0000

    将log类变成打印一个文件

--- src/slave/test/test_log/fl_log.h
+++ src/slave/test/test_log/fl_log.h
@@ -56,6 +56,8 @@ inline void screen_output(const char* fmt, ...)
 // log levelµÄÊýÁ¿
 #define LOG_LEVEL_MAXNUM        10
 
+#define LOG_MSG_SIZE            1024
+
 typedef enum _Log_Level_
 {
     LM_ALL          = 0,
@@ -85,8 +87,8 @@ class CFLLog
         int WriteLog(LogLevel logLevel, const char* logFormat, ...);
 
     private:
-        int OpenLogFile(LogLevel logLevel);
-        int GetLogFileName(char* logFile, int iLen,LogLevel logLevel);
+        int OpenLogFile();
+        int GetLogFileName(char* logFile, int iLen);
         int ShiftLogFiles(LogLevel logLevel, unsigned long maxsize, unsigned short maxnum);
 
     private:
@@ -96,7 +98,7 @@ class CFLLog
         char m_LogName[64];
         unsigned long m_LogSize;
 
-        LogFileInfo m_LogFileInfos[LOG_LEVEL_MAXNUM];
+        LogFileInfo m_LogFileInfo;
 
     public:
         static CFLLog* m_instance;
