7ec35325d6d2f5f3c2bd874d47e6caa0226fc9cd
zny2008 <zny2008@b9611eb2-d0e1-e8f3-2d0a-be6844a88301>
2011-07-11 10:11:48 +0000

    新版django

--- src/master/mysite/settings.py
+++ src/master/mysite/settings.py
@@ -33,7 +33,7 @@ TIME_ZONE = 'GMT-8'
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
 #LANGUAGE_CODE = 'en-us'
-LANGUAGE_CODE = 'zh-CN'
+TIME_ZONE = 'Asia/Shanghai'
 
 SITE_ID = 1
 
