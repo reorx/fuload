06b5f2b0cfe73c10f11edaca0d168fd15f9b09ff
zny2008 <zny2008@b9611eb2-d0e1-e8f3-2d0a-be6844a88301>
2011-07-16 05:58:44 +0000

    新的目录规则

---
+++ trunk/src/master/mysite/uws_app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+#=============================================================================
+#
+#     FileName: uws_app.py
+#         Desc: 这个就是用来启动uwsgi，放到mysite目录下就行
+#
+#       Author: dantezhu
+#        Email: zny2008@gmail.com
+#     HomePage: http://www.vimer.cn
+#
+#      Created: 2011-07-12 00:29:09
+#      Version: 0.0.1
+#      History:
+#               0.0.1 | dantezhu | 2011-07-12 00:29:09 | initialization
+#
+#=============================================================================
+'''
+
+import sys
+import os
+
+#这里一定要到上一级目录，因为setting.py里面都是用的mysite.*之类的
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'../')))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+import django.core.handlers.wsgi
+
+application = django.core.handlers.wsgi.WSGIHandler()
