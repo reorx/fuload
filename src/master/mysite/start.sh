abac8ed6aa1992c1d1c2cdbe6b7442d93bb644c3
zny2008 <zny2008@b9611eb2-d0e1-e8f3-2d0a-be6844a88301>
2011-07-21 01:25:49 +0000

    停止启动脚本

--- src/master/mysite/start.sh
+++ src/master/mysite/start.sh
@@ -4,7 +4,6 @@ cd $run_dir
 
 export PYTHON_EGG_CACHE=/tmp/.python-eggs/
 
-#uwsgi -s :9090 -w uws_app -p10 -d uws.error &
-uwsgi -s :9090 -w uws_app -p10 -d uws.error --touch-reload=uws.tc &
+uwsgi -s :9090 -w uws_app -p10 -d log/uws.error --pidfile uws.pid --touch-reload=uws.tc -L &
 
 #uwsgi --module='django.core.handlers.wsgi:WSGIHandler()' --env DJANGO_SETTINGS_MODULE=mysite.settings -s :9090 -p 10 -d uws.log --touch-reload=uws.tc &
