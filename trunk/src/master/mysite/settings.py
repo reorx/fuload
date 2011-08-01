2271c1c0da5d8f311cb4b021fc3c5432cfcd64fd
zny2008 <zny2008@b9611eb2-d0e1-e8f3-2d0a-be6844a88301>
2011-08-01 16:28:35 +0000

    templates

--- trunk/src/master/mysite/settings.py
+++ trunk/src/master/mysite/settings.py
@@ -82,7 +82,7 @@ ROOT_URLCONF = 'mysite.urls'
 
 from os.path import abspath, dirname, join
 TEMPLATE_DIRS = (
-    abspath(join(dirname(__file__), "template/")),
+    abspath(join(dirname(__file__), "templates/")),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
